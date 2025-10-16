14adae9ff7ca7aea3e16b9a212edc5ddd7b4b967
André Andrade Baceti <a.baceti@murabei.com>
2025-10-16 12:34:48 -0300

    User docker-entry of the image to deploy flask app

--- image-postgis/push-image.sh
+++
@@ -1,8 +0,0 @@
-source version
-git add --all
-git commit -m "Building a new version ${VERSION}"
-git tag -a ${VERSION} -m "Building a new version  ${VERSION}"
-git push
-git push origin ${VERSION}
-
-docker push andrebaceti/regen-timescale-db:${VERSION}
--- image-postgis/start_container.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source wait-for-kong.bash
-
-echo "Starting Flask server"
-flask run --host=0.0.0.0 --port=5000 &
-
-echo "Starting postgres server"
-postgres ${POSTGRES_EXTRA_ARGS}
